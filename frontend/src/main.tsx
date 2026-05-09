ba17c0cb0bcc03c6f4ccebb5ec99ffa432ef18f6
wang-two <3296219847@qq.com>
2026-05-09 16:05:40 +0800

    fix: remove Chinese locale settings from frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ConfigProvider, theme } from 'antd';
-import zhCN from 'antd/locale/zh_CN';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ConfigProvider
-      locale={zhCN}
       theme={{
         algorithm: theme.defaultAlgorithm,
         token: {
